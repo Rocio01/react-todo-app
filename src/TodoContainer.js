55c479702d9854428ffda80996b18937d32a7803
Zulma Rocio <zrmartinezg@gmail.com>
2021-10-25 21:38:43 -0500

    Add UUID

--- src/TodoContainer.js
+++ src/TodoContainer.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Header from './Header';
 import TodosList from './TodosList';
 import InputTodo from './InputTodo';
@@ -12,17 +13,17 @@ export default class TodoContainer extends Component {
     this.state = {
       todos: [
         {
-          id: 1,
+          id: uuidv4(),
           title: 'Setup development environment',
           completed: true,
         },
         {
-          id: 2,
+          id: uuidv4(),
           title: 'Develop website and add content',
           completed: false,
         },
         {
-          id: 3,
+          id: uuidv4(),
           title: 'Deploy to live server',
           completed: false,
         },
@@ -58,7 +59,7 @@ export default class TodoContainer extends Component {
 
   addTodoItem = (title) => {
     const newTodo = {
-      id: 4,
+      id: uuidv4(),
       title,
       completed: false,
     };
